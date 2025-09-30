aa4c2f8f9628277fb7533aa3a7eff9117ea354e6
sunshine-web-coder <exydevcoder@gmail.com>
2025-09-30 21:01:30 +0100

    fix: Update grid layout in ProductDesign component for better responsiveness

--- src/components/sections/home/work-section/works-contents/product-design/ProductDesign.tsx
+++ src/components/sections/home/work-section/works-contents/product-design/ProductDesign.tsx
@@ -3,7 +3,7 @@ import ProductCard from '../ProductCard';
 
 export default function ProductDesign() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 gap-x-1.5 gap-y-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-x-1.5 gap-y-6">
       {productDesignData.map(item => (
         <ProductCard key={item.id} id={item.id}  coverImg={item.coverImg} tags={item.tags} />
       ))}
